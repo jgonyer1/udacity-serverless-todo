1e9833915a52aa4eb6110790fa21057077637c64
Jacob Gonyer <JGonyer@darlingconsulting.com>
2020-10-22 11:35:21 -0400

    Readding semi useful console logs. Also got PUT request body validation in

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -5,8 +5,9 @@ import {createTodo} from '../../businessLogic/todos'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  
   const newItem = await createTodo(newTodo, event.headers.Authorization.split(' ')[1]);
-  // TODO: Implement creating a new TODO item
+  
   return {
     statusCode: 201,
     headers: {
